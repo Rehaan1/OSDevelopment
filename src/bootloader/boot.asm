df10574c04435ca8ac56ab4dae1c21edf13b81ff
Rehaan1 <rehaanmazid@gmail.com>
2023-08-11 22:41:22 +0530

    feat: lba to chs disk read

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -61,6 +61,7 @@ puts:
 	; save registers we will modify
 	push si
 	push ax
+	push bx
 
 .loop:
 	; loadsb, loadsw, loadsd: these instructions load a byte/word/doubleword from ds:si into
@@ -77,6 +78,7 @@ puts:
 	jmp .loop
 	
 .done:
+	pop bx
 	pop ax
 	pop si
 	ret
@@ -92,20 +94,148 @@ main:
 	mov ss, ax
 	mov sp, 0x7C00 ; stack grows downwards from where we are loaded in memory, making sure we dont override OS
 
+	; read something from floppy disk
+	; BIOS should set DL to drive number
+	mov [ebr_drive_number], dl
+
+	mov ax, 1  ; LBA=1, second sector from disk
+	mov cl, 1  ; 1 sector to read
+	mov bx, 0x7E00 ; data should be after the bootloader 
+	call disk_read
 
 	; print message
 	mov si, msg_hello
 	call puts
 	
 	; hlt stops cpu from executing and can be resumed by an interrupt
+	
+	cli ; disable interrupts
 	hlt
 
-.halt:
-	; jmp jumps to given location
-	jmp .halt
+;
+; Error Handlers
+;
 
+floppy_error:
+	mov si, msg_read_failed
+	call puts
+	jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+	mov ah, 0
+	int 16h		; wait for keypress
+	jmp 0FFFFh:0 ; jump to beginning of BIOS, should reboot
+
+.halt:
+	cli			; disable interrupts, this way we cant get out of halt state
+	hlt
+
+
+
+;
+; Disk Routines
+;
+
+;
+; Converts an LBA address to a CHS address
+; Parameters:
+; 	- ax: LBA address
+; Returns:
+;   - cx [bits 0-5]: sector number
+; 	- cs [bits 6-15]: cylinder
+;	- dh: head
+
+lba_to_chs:
+
+	push ax
+	push dx
+
+	xor dx, dx	; dx = 0
+	div word [bdb_sectors_per_track] ; ax = LBA / SectorsPerTrack
+									 ; dx = LBA % SectorsPerTrack
+	inc dx							 ; dx = (LBA % SectorsPerTrack + 1) = sector
+	mov cx, dx						 ; cx = sector
+
+	xor dx, dx	; dx = 0
+	div word [bdb_heads]			 ; ax = (LBA / SectorsPerTrack) / Heads = cylinder
+									 ; dx = (LBA / SectorsPerTrack) % Heads = head
+	mov dh, dl					 ; dh = head
+	mov ch, al						 ; ch = cylinder (lower 8 bits)
+	shl ah, 6  						 
+	or cl, ah						 ; put upper 2 bits of cylinder in CL
+
+	pop ax
+	mov dl, al						 ; restor dl
+	pop ax
+	ret
+
+;
+; Reads Sectors from a Disk
+; Parameters:
+; 	- ax: LBA address
+;   - cl: number of sectors to read (upto 128)
+;   - dl: drive number
+;   - es:bx: memory address where to store read data
+disk_read:
+	
+	push ax			; save registers we will modify
+	push bx
+	push cx
+	push dx
+	push di
+	
+	push cx			; temporarilu save cl (number of sectors to read)
+	call lba_to_chs ; compute chs
+	pop ax			; al = number of sectors to read
+	
+	mov ah, 02h
+	mov di, 3		; retry count
+
+.retry:
+	pusha			; save all registers, we dont know what bios modifies
+	stc				; set carry flag
+	int 13h			; carry flag cleared = success
+	jnc .done
+
+	;read failed
+	popa
+	call disk_reset
+
+	dec di
+	test di, di
+	jnz .retry
+
+.fail:
+	; after all attempts
+	jmp floppy_error
+
+.done:
+	popa
+
+	pop di			; restore registers
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	
+	ret
+
+;
+; Resets disk controllers
+; Parameters:
+;	dl: drive number
+;
+disk_reset:
+	pusha
+	mov ah, 0
+	stc
+	int 13h
+	jc floppy_error
+	popa
+	ret
 
 msg_hello: db 'Hello World!', ENDL, 0
+msg_read_failed: db 'Read From Disk Failed!', ENDL, 0
 
 ; bios expects that last two bytes of the first sector is AA55
 ; we are putting it in standard floppy disk
