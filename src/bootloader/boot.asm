606f006b6aa8d67419b63b9747c58e47c2406bbb
Rehaan1 <rehaanmazid@gmail.com>
2023-08-12 17:20:25 +0530

    feat: load kernel from bootloader

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -48,7 +48,205 @@ ebr_system_id:	db 'FAT12   ' ; 8 bytes
 ;
 
 start:
-	jmp main
+	; setup data segments
+	mov ax, 0 ; can't write to ds/es directly
+	mov ds, ax
+	mov es, ax
+
+	; setup stack
+	mov ss, ax
+	mov sp, 0x7C00 ; stack grows downwards from where we are loaded in memory, making sure we dont override OS
+
+	; some BIOSes might start us at 07C0:0000 instead of 0000:7C00
+	; make sure we are in the expected location
+	push es
+	push word .after
+	retf
+
+.after:
+
+
+	; read something from floppy disk
+	; BIOS should set DL to drive number
+	mov [ebr_drive_number], dl
+
+	; show loading message
+	mov si, msg_loading
+	call puts
+
+	; read drive parameters using BIOS routine Interrupt (sectors per track and head count)
+	; instead of relying on data from formatted disk letting BIOS tell us
+	push es
+	move ah, 08h
+	int 13h
+	jc floppy_error
+	pop es
+
+	and cl, 0x3F ; remove top 2 bits
+	xor ch, ch
+	mov [bdb_sectors_per_track], cx ; sectors count
+
+	inc dh
+	mov [bdb_heads], dh ; head count
+
+	; read FAT root directory
+	; note: this section can be hardcoded
+	mov ax, [bdb_sectors_per_fat] ; LBA of root directory = reserved + fats * fat_size
+	mov bl, [bdb_fat_count]
+	xor bh, bh
+	mul bx ; ax = (fats * sectors_per_fat)
+	add ax, [bdb_reserved_sectors] ; ax = LBA of root directory
+	push ax
+
+	; compute size of root directory = (32 * number of entries) / bytes per sector
+	mov ax, [bdb_sectors_per_fat] 
+	shl ax, 5 ; ax *= 32
+	xor dx, dx ; dx = 0
+	div word [bdb_bytes_per_sector] ; number of sectors we need to read
+
+	test dx, dx ; if dx != 0, add 1
+	jz .root_dir_after
+	inc ax ; division remaining != 0, add 1
+		   ; this means we have a sector only partially filled
+
+.root_dir_after:
+
+	; read root directory
+	mov cl, al ; cl = number of sectors to read = size of root directory
+	pop ax ; ax = LBA of root directory
+	mov dl, [ebr_drive_number] ; dl = drive number
+	mov bx, buffer ; ex:bx = buffer
+	call disk_read
+
+
+	; search for kernel.bin
+	xor bx, bx
+	mov di, buffer
+
+.search_kernel:
+	mov si, file_kernel_bin
+	mov cx, 11 ; compare upto 11 characters
+	push di
+	repe cmpsb ; repe repeats while equal upto cx times and cx is decremented, cmpsb compares two bytes in memory one stored in ds:si and es:di
+	pop di
+	je .found_kernel
+
+	; move to next directory entry
+	add di, 32 ; 32 is size of directory entry
+	inc bx
+	cmp bx, [bdb_dir_entries_count]
+	jl .search_kernel
+
+	; kernel not found
+	jmp kernel_not_found_error
+
+.found_kernel:
+	
+	; di should have the address to the entry
+	mov ax, [di + 26] ; first logical cluster field (offset 26)
+	mov [kernel_cluster], ax
+
+	; load FAT from disk into memory
+	mov ax, [bdb_reserved_sectors]
+	mov bx, buffer
+	mov cl, [bdb_sectors_per_fat]
+	mov dl, [ebr_drive_number]
+	call disk_read
+
+	; read kernel and process FAT chain
+	mov bx, KERNEL_LOAD_SEGMENT
+	mov es, bx
+	mov bx, KERNEL_LOAD_OFFSET
+
+.load_kernel_loop:
+
+	; Read next cluster
+	mov ax, [kernel_cluster]
+
+	; TODO: Change Hardcoded value
+	add ax, 31 ; first cluster = start_sector + (kernel_cluster - 2 ) * sectors_per_cluster
+			   ; start_sector = reserved + fats + root directory size = 1 + 18 + 134 = 33
+	mov cl, 1
+	mov dl, [ebr_drive_number]
+	call disk_read
+
+	add bx, [bdb_bytes_per_sector]
+	
+	; compute location of next cluster
+	mov ax, [kernel_cluster]
+	mov cx, 3
+	mul cx
+	mov cx, 2
+	div cx ; ax = index of entry in fAT, dx = cluster mod 2
+
+	mov si, buffer
+	add si, ax
+	mov ax, [ds:si] ; read entry from FAT Table at index ax
+
+	or dx, dx
+
+	jz .even
+
+.odd:
+	shr ax, 4
+	jmp .next_cluster_after
+
+.even:
+	and ax, 0x0FFF
+
+.next_cluster_after:
+	cmp ax, 0x0FF8  ; check end of chain
+	jae .read_finish
+
+	mov [kernel_cluster], ax
+	jmp .load_kernel_loop
+
+.read_finish:
+	
+	; jump to our kernel
+	mov dl, [ebr_drive_number] ; boot device in dl
+
+	; set segment registers
+	mov ax, KERNEL_LOAD_OFFSET
+
+	mov ds, ax
+	mov es, ax
+
+	jmp KERNEL_LOAD_SEGMENT:KERNEL_LOAD_OFFSET
+
+
+	; if jump fails
+	jmp wait_key_and_reboot
+
+
+
+	; hlt stops cpu from executing and can be resumed by an interrupt
+	
+	cli ; disable interrupts
+	hlt
+
+;
+; Error Handlers
+;
+
+floppy_error:
+	mov si, msg_read_failed
+	call puts
+	jmp wait_key_and_reboot
+
+kernel_not_found_error:
+	mov si, msg_kernel_not_found
+	call puts
+	jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+	mov ah, 0
+	int 16h		; wait for keypress
+	jmp 0FFFFh:0 ; jump to beginning of BIOS, should reboot
+
+.halt:
+	cli			; disable interrupts, this way we cant get out of halt state
+	hlt
 
 
 ;
@@ -82,54 +280,6 @@ puts:
 	pop ax
 	pop si
 	ret
-
-main:
-
-	; setup data segments
-	mov ax, 0 ; can't write to ds/es directly
-	mov ds, ax
-	mov es, ax
-
-	; setup stack
-	mov ss, ax
-	mov sp, 0x7C00 ; stack grows downwards from where we are loaded in memory, making sure we dont override OS
-
-	; read something from floppy disk
-	; BIOS should set DL to drive number
-	mov [ebr_drive_number], dl
-
-	mov ax, 1  ; LBA=1, second sector from disk
-	mov cl, 1  ; 1 sector to read
-	mov bx, 0x7E00 ; data should be after the bootloader 
-	call disk_read
-
-	; print message
-	mov si, msg_hello
-	call puts
-	
-	; hlt stops cpu from executing and can be resumed by an interrupt
-	
-	cli ; disable interrupts
-	hlt
-
-;
-; Error Handlers
-;
-
-floppy_error:
-	mov si, msg_read_failed
-	call puts
-	jmp wait_key_and_reboot
-
-wait_key_and_reboot:
-	mov ah, 0
-	int 16h		; wait for keypress
-	jmp 0FFFFh:0 ; jump to beginning of BIOS, should reboot
-
-.halt:
-	cli			; disable interrupts, this way we cant get out of halt state
-	hlt
-
 
 
 ;
@@ -234,8 +384,14 @@ disk_reset:
 	popa
 	ret
 
-msg_hello: db 'Hello World!', ENDL, 0
+msg_loading: db 'Loading UTOPIAN Operating System....', ENDL, 0
 msg_read_failed: db 'Read From Disk Failed!', ENDL, 0
+msg_kernel_not_found: db 'KERNEL.BIN : UTOPIAN Kernel Not Found!', ENDL, 0
+file_kernel_bin: db 'KERNEL  BIN'
+kernel_cluster: dw 0
+
+KERNEL_LOAD_SEGMENT	equ 0x2000
+KERNEL_LOAD_OFFSET	equ 0
 
 ; bios expects that last two bytes of the first sector is AA55
 ; we are putting it in standard floppy disk
@@ -249,3 +405,5 @@ msg_read_failed: db 'Read From Disk Failed!', ENDL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
+
+buffer:
--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,21 +1,21 @@
-; org directive tells the assembler where we expect our code to be loaded. In our case since
-; we are using legacy bios booting mode we want it on location 0x7C00
-
-; directive gives clue to assembler and is not translated to machine code
-; a instruction is translated into machine code
-
-org 0x7C00
+; org directive tells the assembler where we expect our code to be loaded
+org 0x0
 
 ; tell assembler to emit 16 bit code
-
 bits 16
 
 
 %define ENDL 0x0D, 0x0A
 
 start:
-	jmp main
+	; print message
+	mov si, msg_hello
+	call puts
 
+.halt:	
+	; hlt stops cpu from executing and can be resumed by an interrupt
+	cli
+	hlt
 
 ;
 ; Prints a string to the screen
@@ -27,6 +27,7 @@ puts:
 	; save registers we will modify
 	push si
 	push ax
+	push bx
 
 .loop:
 	; loadsb, loadsw, loadsd: these instructions load a byte/word/doubleword from ds:si into
@@ -36,52 +37,17 @@ puts:
 	or al, al	; verify if next character is null
 	jz .done ; jz : jumps to destination if zero flag is set
 
-	mov ah, 0x0e
+	mov ah, 0x0E
 	mov bh, 0
 	int 0x10 ; triggers a software interrupt
 
 	jmp .loop
 	
 .done:
+	pop bx
 	pop ax
 	pop si
 	ret
 
-main:
 
-	; setup data segments
-	mov ax, 0 ; can't write to ds/es directly
-	mov ds, ax
-	mov es, ax
-
-	; setup stack
-	mov ss, ax
-	mov sp, 0x7C00 ; stack grows downwards from where we are loaded in memory, making sure we dont override OS
-
-
-	; print message
-	mov si, msg_hello
-	call puts
-	
-	; hlt stops cpu from executing and can be resumed by an interrupt
-	hlt
-
-.halt:
-	; jmp jumps to given location
-	jmp .halt
-
-
-msg_hello: db 'Hello World!', ENDL, 0
-
-; bios expects that last two bytes of the first sector is AA55
-; we are putting it in standard floppy disk
-
-; db directive writes given bytes to the assembled binary file
-; times directive used to repeate given instructions or data
-; dollar sign which is equal to the memory offset of the current line
-; double dollar sign which is equal to the memory offset of the beginning of the current section
-
-; dolar - dollar dollar gives the size of the program so far in bytes
-
-times 510-($-$$) db 0
-dw 0AA55h
+msg_hello: db 'Hello World From UTOPIAN Kernel!', ENDL, 0
